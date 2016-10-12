e69f5e6b9b59656aee72126ca4d11ed7918ca0eb
Kenneth Palaganas <kenneth@jotform.com>
2016-10-12 23:52:14 +0800

    test

--- app/controllers/botkit.js
+++ app/controllers/botkit.js
@@ -64,32 +64,32 @@ controller.hears(['check', 'submissions'], 'message_received', function(bot, mes
       var formID = response.text;
       convo.say('Cool, I\'ll be back in sec, I will check your form with ID: ' + formID);
 
-      setTimeout(function() {
-        convo.say(message, 'Here\'s the submissions of your form.');
-        convo.say('Form ID: '+formID+' and Submission as of 10/12/2016');
+      // setTimeout(function() {
+      //   convo.say(message, 'Here\'s the submissions of your form.');
+      //   convo.say('Form ID: '+formID+' and Submission as of 10/12/2016');
 
-        convo.ask('Anything else?', [{
-          pattern: bot.utterances.no,
-          callback: function(response,convo) {
-            convo.say('OK see you next time!');
-            convo.next();
-          }
-        }, {
-          pattern: bot.utterances.yes,
-          callback: function(response,convo) {
-            convo.say('Great! ask away...');
-            // do something else...
-            convo.next();
-          }
-        }, {
-          default: true,
-          callback: function(response,convo) {
-            // just repeat the question
-            convo.say('OK I think that\'s a NO, see yah!');
-            convo.next();
-          }
-        }]);
-      }, 3000);
+      //   convo.ask('Anything else?', [{
+      //     pattern: bot.utterances.no,
+      //     callback: function(response,convo) {
+      //       convo.say('OK see you next time!');
+      //       convo.next();
+      //     }
+      //   }, {
+      //     pattern: bot.utterances.yes,
+      //     callback: function(response,convo) {
+      //       convo.say('Great! ask away...');
+      //       // do something else...
+      //       convo.next();
+      //     }
+      //   }, {
+      //     default: true,
+      //     callback: function(response,convo) {
+      //       // just repeat the question
+      //       convo.say('OK I think that\'s a NO, see yah!');
+      //       convo.next();
+      //     }
+      //   }]);
+      // }, 3000);
     });
   });
 });
